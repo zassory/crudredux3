e605a71d9f5dc93b30f933e06c427c226751e3d9
zassory <nicolas.programador@gmail.com>
2023-03-14 17:37:44 -0300

    header listo

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary
+    justify-content-between">
+        <div className='container'>
+            <h1 className='text-center'>CRUD - React, Redux, REST API & Axios</h1>
+        </div>
+
+        <a 
+            href="/productos/nuevo"
+            className='btn btn-danger nuevo-post d-block d-md-inline-block'    
+        >Agregar Producto &#43;</a>
+    </nav>
+  )
+}
+
+export default Header;
