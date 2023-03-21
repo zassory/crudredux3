0dce8bdf58484cc223a8affca41d760b5ad59aca
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-03-21 17:38:56 -0300

    store creado

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { Header , Productos , NuevoProducto , EditarProducto } from './component
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
     <Router>
-      <Header />
+      <Provider store={store}>
+        <Header />
 
-      <div className="container mt-5">
-        <Routes>
-          <Route path="/" element={<Productos />} />
-          <Route path="/productos/nuevo" element={<NuevoProducto />} />
-          <Route path="/productos/editar/:id" element={<EditarProducto />} />
-        </Routes>
-      </div>
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<Productos />} />
+            <Route path="/productos/nuevo" element={<NuevoProducto />} />
+            <Route path="/productos/editar/:id" element={<EditarProducto />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
   );
 }
